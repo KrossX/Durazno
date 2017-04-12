9b5c3fee2b24666da32046020fc55e73af88ee50
KrossX <krossx@yahoo.com>
2017-04-12 16:28:55 -0300

    DLL rewritten in C part1

--- Durazno/FileIO.h
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2012 KrossX <krossx@live.com>
- * License: http://www.opensource.org/licenses/mit-license.html  MIT License
- */
-
-#pragma once
-
-void INI_LoadSettings(SETTINGS *settings);
-void INI_SaveSettings(SETTINGS *settings);
-void INI_ReloadSettings();
